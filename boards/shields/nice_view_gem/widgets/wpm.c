6ac961dc8f6a16246f8ace26a2ee9fa64679fe89
陆离 <sirzenith@163.com>
2025-12-30 14:53:19 +0800

    fix: update definition name

--- boards/shields/nice_view_gem/widgets/wpm.c
+++ boards/shields/nice_view_gem/widgets/wpm.c
@@ -7,7 +7,7 @@ LV_IMG_DECLARE(gauge);
 LV_IMG_DECLARE(grid);
 
 static void draw_gauge(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_image_dec_t img_dsc;
+    lv_draw_image_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
     lv_canvas_draw_img(canvas, 16, 44 + BUFFER_OFFSET_MIDDLE, &gauge, &img_dsc);
@@ -53,7 +53,7 @@ static void draw_needle(lv_obj_t *canvas, const struct status_state *state) {
 }
 
 static void draw_grid(lv_obj_t *canvas) {
-    lv_draw_image_dec_t img_dsc;
+    lv_draw_image_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
     lv_canvas_draw_img(canvas, 0, 65 + BUFFER_OFFSET_MIDDLE, &grid, &img_dsc);
