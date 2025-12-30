b92a0e6c1c52a12d7fa3930b0ba33b7194ec3276
陆离 <sirzenith@163.com>
2025-12-30 16:38:27 +0800

    fix: update drawing api to lvglv9

--- boards/shields/nice_view_gem/widgets/util.c
+++ boards/shields/nice_view_gem/widgets/util.c
@@ -19,15 +19,30 @@ void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
     img.header.h = BUFFER_SIZE;
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, BUFFER_SIZE / 2,
-                        BUFFER_SIZE / 2, false);
+    /* lv_canvas_transform(canvas, &img, 900, LV_ZOOM_NONE, -1, 0, BUFFER_SIZE / 2, BUFFER_SIZE / 2,
+                        false); */
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_draw_image_dsc_t img_dsc;
+    lv_draw_image_dsc_init(&img_dsc);
+    img_dsc.rotation = 900;
+    img_dsc.src = &img;
+    img_dsc.pivot.x = BUFFER_SIZE / 2;
+    img_dsc.pivot.y = BUFFER_SIZE / 2;
+
+    lv_area_t coords_img = {0, 0, BUFFER_SIZE - 1, BUFFER_SIZE - 1};
+    lv_draw_image(&layer, &img_dsc, &coords_img);
+
+    lv_canvas_finish_layer(canvas, &layer);
 }
 
 void fill_background(lv_obj_t *canvas) {
-    lv_draw_rect_dsc_t rect_black_dsc;
-    init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
+    // lv_draw_rect_dsc_t rect_black_dsc;
+    // init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
 
-    lv_canvas_draw_rect(canvas, 0, 0, BUFFER_SIZE, BUFFER_SIZE, &rect_black_dsc);
+    // lv_draw_rect(canvas, 0, 0, BUFFER_SIZE, BUFFER_SIZE, &rect_black_dsc);
+    lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
 }
 
 void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
